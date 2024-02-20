0c48c3e8831e8771374b69c0e6cc41ac487f68a7
EsanDan <esanod@mail.uc.edu>
2024-02-19 23:13:15 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String trash;
+        boolean done = false;
+
+        int randGuess = 0;
+
+        Random generator = new Random();
+        int randInt = generator.nextInt(10);
+
+        do {
+            System.out.println("Guess the random number [1-10]: ");
+
+            if(in.hasNextInt()) {
+                randGuess = in.nextInt();
+                in.nextLine();
+
+                if (randGuess >= 1 && randGuess <= 10) {
+                    done = true;
+                } else {
+                    System.out.println("You must input a valid guess between 1 and 10, not: " + randGuess);
+                }
+                System.out.println("Your guess was " + randGuess);
+
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("You must input a valid guess, not" + trash);
+            }
+
+        } while (!done);
+
+        System.out.println("The random number was " + randInt);
+
+        if(randInt == randGuess)
+        {
+            System.out.println("You're right on the money!");
+        } else if (randInt > randGuess) {
+            System.out.println("Your guess was low.");
+        }
+        else
+        {
+            System.out.println("Your guess was high.");
+        }
+
+    }
+}
